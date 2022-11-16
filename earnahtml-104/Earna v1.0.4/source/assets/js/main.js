1e5245f5821fac216954bbe4fb15fd4755b33ae9
Mohamad Meerasa <mohamadmeerasa45@gmail.com>
2022-11-16 11:35:05 +0530

    16/11

--- earnahtml-104/Earna v1.0.4/source/assets/js/main.js
+++ earnahtml-104/Earna v1.0.4/source/assets/js/main.js
@@ -124,6 +124,18 @@
 
         });
 
+        $(document).ready(function () {
+            $('.nav li').click(function(e) {
+        
+                $('.nav li').removeClass('active');
+        
+                var $this = $(this);
+                if (!$this.hasClass('active')) {
+                    $this.addClass('active');
+                }
+                //e.preventDefault();
+            });
+        });
 
          /* ==================================================
             # Fun Factor Init
